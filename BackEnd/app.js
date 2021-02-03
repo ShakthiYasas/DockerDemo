682769ba17549f058e988198d5d42b12ccb7ac27
Shakthi Weerasinghe <yasas276@gmail.com>
2021-02-03 11:06:19 +0530

    get products added

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,6 +14,11 @@ app.get('/',(req, res) => {
     res.json('Hey! Welcome to the Docker Demo with Version Controlling!');
 });
 
+app.get('/products',(req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json('This is the product endpoint template!');
+});
+
 app.listen(5000, () => {
     // console.log('Demo started!');
     console.log('Demo Started using the current version: ' + process.env.VERSION +'!');
